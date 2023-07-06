5f64d94826b16a5721e82962e1d47baf170a4920
Davies Aniefiok <daviesaniefiok32@gmail.com>
2023-07-06 19:43:15 +0000

    added task 8

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""this is a type annotated module"""
+from typing import Callable
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """takes a float amd returns a function that multiplies a flaat
+    by multiplier
+    """
+    def multiply(x: float) -> float:
+        "''takes a float and returns a float"""
+        return x * multiplier
+    return multiply
