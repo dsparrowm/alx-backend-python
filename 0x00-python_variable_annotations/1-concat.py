96489ab38eaa513b5c7bff1ea45a94b6cad5f70b
Davies Aniefiok <daviesaniefiok32@gmail.com>
2023-07-06 18:19:18 +0000

    added task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""this module is type-annotated"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """this function takes two string parameters and returns a
+    concatenated string
+    """
+    return str1 + str2
