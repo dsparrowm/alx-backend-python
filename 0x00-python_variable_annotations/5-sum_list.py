c718063cac90e76c8e5fb96eac4c323451c21ee4
Davies Aniefiok <daviesaniefiok32@gmail.com>
2023-07-07 06:29:10 +0000

    i added a blank line after module documentation

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,6 +2,7 @@
 """this is a type-annotated module"""
 from typing import List
 
+
 def sum_list(input_list: List[float]) -> float:
     """this function takes a list of floats and returns their sum
     as a float"""
