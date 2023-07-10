868b8f2dde13d0591191e79767259b555639e339
Davies Aniefiok <daviesaniefiok32@gmail.com>
2023-07-10 12:48:05 +0000

    added task 1

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""this module handles concurrent codes"""
+import asyncio
+import random
+from typing import List
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """this coroutine takes in two int values and returns a list
+    of all the delays
+    """
+    result = [wait_random(max_delay) for _ in range(n)]
+    new_result = await asyncio.gather(*result)
+    return new_result
